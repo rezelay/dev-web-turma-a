d2243e8c61e1150c94c7fa0f9ec3640c37dd00f7
rezelay <laaysouza.r@gmail.com>
2026-03-26 21:07:49 -0300

    Criação de componentes no VUE

--- Layara Souza Rezende/Aulas/vue_cli/src/App.vue
+++ Layara Souza Rezende/Aulas/vue_cli/src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Muda na hora"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>meu App Vue</h1>
+    <p class="subtitulo">Meu Subtitulo</p>
+    <br />
+    <home-component />
+    <br />
+    <usuario-component />
+    <br />
+    <estilo-component />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from "./components/HomeComponent.vue";
+import UsuarioComponent from "./components/UsuarioComponent.vue";
+import EstiloComponent from "./components/EstiloComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomeComponent,
+    UsuarioComponent,
+    EstiloComponent
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  created() {
+    console.log("created");
+  },
+  updated() {
+    console.log("updated");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+};
 </script>
 
 <style>
@@ -23,4 +48,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/* h1{
+  color: darkmagenta;
+  text-align: center;
+  }
+  .subtitulo{
+    color: darkgreen;
+    font-family: "Gill Sans";
+    text-align: center; */
 </style>
